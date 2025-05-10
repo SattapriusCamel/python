efe4a25b875b7e8850ecd785063cbcd8958204a0
SattapriusCamel <oikkoandfriends90@gmail.com>
2025-05-10 20:54:08 +0200

    data types

---
+++ data types/d.py
@@ -0,0 +1,9 @@
+name = "Micheal Myers"
+walkspeed = 16
+Never_Runs = True
+weight = 17.38
+
+print("I swear ", name, " be sprinting off camera")
+print("Ong ", int(weight))
+print("OFN my walkspeed always be ", float(walkspeed),"jus like in roblox. I am always", Never_Runs)
+print("Data type of walkspeed is", type(walkspeed))
