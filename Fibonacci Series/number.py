cee8d6e0f3e1fab8b9f42b90123e3f85527cdb97
SattapriusCamel <oikkoandfriends90@gmail.com>
2025-05-27 21:24:34 +0200

    fibonacci

--- Fibonacci Series/number.py
+++ Fibonacci Series/number.py
@@ -1,4 +1,14 @@
-print("The Fibonnaci series goes like this: ")
+print("The Fibonnaci series")
 
-def fibonacci(x,y):
-    
+n=int(input("Enter the times want to the number go into: "))
+fn=0
+sn=1
+nextnum=sn
+ronk=1
+
+while nextnum<= n:
+    print(ronk,end=" ")
+    nextnum += 1
+    fn, sn = sn, ronk
+    ronk = fn + sn
+print()
