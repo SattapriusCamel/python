8c5fe18ac0c4f49b02bc4bfda754486bbcf28d27
SattapriusCamel <oikkoandfriends90@gmail.com>
2025-06-21 16:55:11 +0200

    number expression

---
+++ expression numbers/num.py
@@ -0,0 +1,9 @@
+def addnum(a,b,c):
+    return a + b + c
+
+num1 = int(input("Enter The First Number: "))
+num2 = int(input("Enter The Second Number: "))
+num3 = int(input("Enter The Third Number: "))
+
+print("The final value is ",addnum(num1, num2, num3))
+        
